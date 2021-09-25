630335e04fcc611e9ca2ed7a49069269dfa6fdf6
Kiley Poole <kpoole007@gmail.com>
2021-09-25 14:58:12 -0700

    instruction reductions

--- sum_to_n.asm
+++ sum_to_n.asm
@@ -1,18 +1,16 @@
 section .text
 global sum_to_n
-sum_to_n_iter:
-		mov 	rax, 0		;
-		mov 	rdx, 0		;
-	loop:
-		add 	rax, rdx	;
-		inc 	rdx	 		;
-		cmp 	rdx, rdi	;
-		jle 	loop		;
-		ret					;
 sum_to_n:
-		mov 	rdx, rdi	;	
-		add 	rdi, 1		;
-		imul 	rdi, rdx	;
-		sar		rdi, 1		;
-		mov 	rax, rdi	;
-		ret					;
+		xor 	rax, rax		;
+		xor 	rdx, rdx		;
+	loop:
+		add 	rax, rdx		;
+		inc 	rdx	 			;
+		cmp 	rdx, rdi		;
+		jle 	loop			;
+		ret						;
+sum_to_n_const:
+		lea 	rax, [rdi + 1]	;	
+		imul 	rax, rdi		;
+		sar		rax, 1			;
+		ret						;
