2d61e451ee37b42c767de197ea11eb739e0d7b9c
Kiley Poole <kpoole007@gmail.com>
2021-09-25 14:46:55 -0700

    added const time sum_of_n

--- sum_to_n.asm
+++ sum_to_n.asm
@@ -1,11 +1,18 @@
 section .text
 global sum_to_n
-sum_to_n:
-		mov rax, 0;
-		mov r8,  0;
+sum_to_n_iter:
+		mov 	rax, 0		;
+		mov 	rdx, 0		;
 	loop:
-		add rax, r8;
-		inc r8;
-		cmp r8, rdi;
-		jle loop;
-		ret
+		add 	rax, rdx	;
+		inc 	rdx	 		;
+		cmp 	rdx, rdi	;
+		jle 	loop		;
+		ret					;
+sum_to_n:
+		mov 	rdx, rdi	;	
+		add 	rdi, 1		;
+		imul 	rdi, rdx	;
+		sar		rdi, 1		;
+		mov 	rax, rdi	;
+		ret					;
